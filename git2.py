9bac5e710cd41c8abdcc0c8f7818a0d893e55a94
Svati Priya <swatipriya98484@gmail.com>
2024-05-26 21:33:52 +0530

    first commit

---
+++ git2.py
@@ -0,0 +1,10 @@
+num1 = float(input("Enter the first number:"))
+num2 = float(input("Enter the second number:"))
+
+print("sum:", num1 + num2)
+print("Difference:", num1 - num2)
+print("product:", num1 * num2)
+if num2 !=0:
+    print("Quotient:", num1 / num2)
+else:
+    print("Quotient: Division by zero!")   
